f045e71968abf83a4cd6d7cca5e5e96b0579200e
miasif <minhazul.islam.asif@gmail.com>
2025-01-14 16:06:00 +0600

    navbar modify

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,24 +1,17 @@
-"use client";
+// components/Navbar.js
 import Image from "next/image";
-import { useState } from "react";
 import NavItem from "./NavItem";
 import ThemeToggle from "./ThemeToggle";
 
 const Navbar = () => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen((prevState) => !prevState);
-  };
-
   return (
-    <nav className="bg-white dark:bg-gray-900 text-gray-800 dark:text-white shadow-md sticky top-0 z-50">
+    <nav className="bg-white dark:bg-gray-900 text-white shadow-md sticky top-0 z-50">
       <div className="container mx-auto flex items-center justify-between px-4 py-4">
         {/* Brand */}
         <div className="flex items-center">
           <a
-            href="#home"
-            className="font-medium text-2xl hover:text-gray-500 dark:hover:text-gray-300 transition"
+            href="index.html"
+            className="text-white font-medium text-2xl hover:text-gray-200 focus:text-gray-200 transition"
           >
             <Image
               src={"/assets/luxary-logo.jpg"}
@@ -27,64 +20,24 @@ const Navbar = () => {
               height={50}
               className="rounded-full"
             />
+            <span className="text-yellow-500">.</span>
           </a>
         </div>
 
-        {/* Menu Button (Hamburger) */}
-        <button
-          onClick={toggleMobileMenu}
-          className="lg:hidden focus:outline-none"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="w-6 h-6 transition-transform duration-300"
-            strokeWidth="2"
-            // Dynamic stroke color for light and dark mode
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </button>
-
         {/* Menu */}
-        <div
-          className={`${
-            isMobileMenuOpen ? "block" : "hidden"
-          } lg:flex flex-col lg:flex-row lg:items-center lg:space-x-8`}
-        >
+        <div className="hidden lg:flex flex-col lg:flex-row lg:items-center lg:space-x-8">
           <ul className="flex flex-col lg:flex-row lg:space-x-8 text-center">
-            <NavItem label="Home" href="#home" />
-            <NavItem label="About" href="#about" />
-            <NavItem label="Packages" href="#featured-packages" />
-            <NavItem label="Gallery" href="#destinations" />
-            <NavItem label="Contact" href="#inquiry-form" />
+            <NavItem label="Home" href="#home" isActive />
+            <NavItem label="New Arrival" href="#new-arrivals" />
+            <NavItem label="Features" href="#feature" />
+            <NavItem label="Blog" href="#blog" />
+            <NavItem label="Contact" href="#newsletter" />
           </ul>
-
-          {/* Theme Toggle Button for Mobile */}
-          <div className="lg:hidden mt-4">
-            <ThemeToggle />
-          </div>
         </div>
 
-        {/* Theme Toggle Button for Desktop */}
-        <div className="hidden lg:block">
-          <ThemeToggle />
-        </div>
+        {/* Theme Toggle Button */}
+        <ThemeToggle />
       </div>
-
-      {/* Mobile Menu Overlay */}
-      {isMobileMenuOpen && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-50 z-40 lg:hidden"
-          onClick={toggleMobileMenu}
-        ></div>
-      )}
     </nav>
   );
 };
